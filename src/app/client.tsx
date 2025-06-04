37b664db5220e935d54bfc38e89c51f7c9a5eed4
Tim Purdum <tim.purdum@dymaptic.com>
2025-06-03 19:10:01 -0500

    commit project

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -1,9 +1,16 @@
 "use client";
 import dynamic from "next/dynamic";
 import "@arcgis/map-components/dist/components/arcgis-map";
+import "@arcgis/map-components/components/arcgis-expand";
 
 const Client = () => (
-      <arcgis-map class="arcgis-map" itemId="d5dda743788a4b0688fe48f43ae7beb9"></arcgis-map>
+      <arcgis-map class="arcgis-map" itemId="d5dda743788a4b0688fe48f43ae7beb9">
+          <arcgis-expand close-on-esc position="bottom-left" mode="floating">
+            <arcgis-placement>
+              
+            </arcgis-placement>
+          </arcgis-expand>
+      </arcgis-map>
   );
 
 export default dynamic(() => Promise.resolve(Client), {ssr: false});
